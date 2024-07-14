c640ef0e66ec643d4e1765d2fa701800fa0c05db
Peter Kim <peter2003kim@gmail.com>
2024-07-13 20:08:22 -0700

    Added ability to write to a new .csv file after reading from original file. Only for the years portion of the data.

--- data_organizer/main.cpp
+++ data_organizer/main.cpp
@@ -4,20 +4,45 @@
 using namespace std;
 
 int main() {
+    // getting file & ticker name data
     string fileName;
+    string tickerName;
+    
     cout << "Enter file name: ";
     cin >> fileName;
     transform(fileName.begin(), fileName.end(), fileName.begin(), ::toupper);
+    tickerName = fileName;
     fileName.append(".csv");
     
-    fstream file;
-    file.open(fileName);
+    // file reading and writing start
+    ifstream rdFile;
+    ofstream wrtFile;
     
-    if (!file.is_open()){
+    rdFile.open(fileName);
+    wrtFile.open("revenue_data.csv");
+    // opening file test
+    if (!rdFile.is_open()){
         cout << "Error opening file" << endl;
     }
     else{
+        // writing list of years into file (first 5 from orig file)
+        string years;
+        string dataMetric;
+        
+        getline(rdFile, years);
+        years.erase(0,4);
+        years.insert(0, "Ticker");
+        years.erase(31, years.size());
+        wrtFile << years << endl;
+        
+        //writing revenue data into file (past 5 years)
+        
+        
+        
+        
         
     }
+    rdFile.close();
+    wrtFile.close();
     
 }
